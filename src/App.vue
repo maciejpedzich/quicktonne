bd51993fc62155dab0039536cf836ef4a5871ad6
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-01 18:35:56 +0100

    Apply flex classes only on specific routes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <script setup lang="ts">
-import ReloadToast from './components/ReloadToast.vue';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const isPageWithCenteredText = computed(() =>
+  ['Home', 'About'].includes(route.name as string)
+);
 </script>
 
 <template>
   <div
-    class="w-full h-full flex flex-column align-items-center justify-content-center"
+    :class="[
+      'w-full',
+      'h-full',
+      isPageWithCenteredText
+        ? 'flex flex-column align-items-center justify-content-center'
+        : ''
+    ]"
   >
     <router-view></router-view>
   </div>
-  <ReloadToast id="reload-toast" />
 </template>
 
 <style>
 body {
   color: var(--text-color);
   font-family: var(--font-family);
-}
-
-#reload-toast .p-toast-message .p-toast-message-content {
-  justify-content: center !important;
 }
 </style>
