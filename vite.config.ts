758684c5be7595be9c82abc33ffdfe6e5c09904b
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-03 22:17:09 +0100

    Remove path.resolve alias replacement URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 import eslintPlugin from 'vite-plugin-eslint';
 import vue from '@vitejs/plugin-vue';
@@ -10,7 +9,7 @@ export default defineConfig({
     alias: [
       {
         find: '@',
-        replacement: path.resolve(__dirname, '/src')
+        replacement: '/src'
       }
     ]
   },
